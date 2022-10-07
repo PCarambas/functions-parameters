870c83a99b46d09145bb94cd7796af5024397ff2
Peggy Carambas <PCarambas@gmail.com>
2022-10-07 10:37:48 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/* 1. Define parameter that will hold original number
+2. Divide number by 4
+3. Return quartered number
+4. Store the number in a variable
+5. Plug that variable into the parenthesis for the console.log()
+6 Run code to see answer in console */
+
+const quarterValue = (scaryNumber) => {
+    const quarter = scaryNumber / 4
+
+    return quarter
+
+}
+
+const output = quarterValue(40)
+
+console.log(output)
